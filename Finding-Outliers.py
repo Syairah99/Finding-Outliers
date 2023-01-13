4ec9d1d1d43a9d8c7685f51abd6f5c45f628d75a
NUR SYAIRAH BINTI MOHD IRWAN <nursyairahirwan@gmail.com>
2023-01-13 16:55:36 +0800

    Add files via upload
    To check if the number is an outlier in the dataset and set the variable outlier to be True if it is.

---
+++ Finding-Outliers.py
@@ -0,0 +1,17 @@
+# %%
+
+# Create variables
+upper_limit = 91
+lower_limit = 24
+outlier = False
+number = 8
+
+# If conditionals
+if number > upper_limit:
+    outlier = True
+if number < lower_limit:
+    outlier = True
+if outlier == True:
+    print(f"{number} is an outlier")
+    
+# %%
